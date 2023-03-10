e7c0a030dd6d4e7cfc61f232b39b7d33df09c983
Ahmed Ali <121734255+AhmedAli4821@users.noreply.github.com>
2023-03-10 23:24:02 +0500

    JS 21 To 25

---
+++ java 21 to 25/app.js
@@ -0,0 +1,49 @@
+//document.write ("<h1>Chapter 21 To 25</h1>")
+
+//document.write("<h3>Question No 1</h3>")
+
+//var a = prompt("First Naqme")
+//var b = prompt("Last Name")
+//alert("Hello"+" "+a+" "+b)
+
+// document.write("<h3>Question No 2</h3>")
+// var a = prompt("Favourit Mobile Model")
+// var b = a.length
+// alert(a+" "+"Lenth Of b:" +b)
+
+//document.write("<h3>Question No 3</h3>")
+//var a = "Pakistani"
+//alert(a.indexOf("n"))
+
+//document.write("<h3>Question No 4</h3>")
+//alert(a.lastIndexOf("1"))
+
+// document.write("<h3>Question No 5</h3>")
+// var a = "Pakistani"
+// alert(a.charAt(3))
+
+// document.write("<h3>Question No 6</h3>")
+// var a = "haydarabad" 
+// var b = a.replace("haydarabad","islamabad")
+// alert(b)
+
+// document.write("<h3>Question No 7</h3>")
+// var a = "Ali and Sami are best friend.They play circket and football together"
+// var b = a.replace("and","&")
+// alert(b)
+
+// document.write("<h3>Question No 8</h3>")
+// var a = 472
+// alert(a.toString())
+
+// document.write("<h3>Question No 9</h3>")
+// var a = "input"
+// alert(a.toUpperCase())
+
+// document.write("<h3>Question No 10</h3>")
+// var a = "peanuts"
+// alert(a.toUpperCase())
+
+document.write("<h3>Question No 11</h3>")
+var a = 35.36
+alert(a.toString())
